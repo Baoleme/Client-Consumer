037b51babe0fd9e293f4bd2133308fc6dbbe69b5
MegrezZhu <mystery490815101@gmail.com>
2018-05-06 14:50:58 +0800

    static: restaurant info

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import user from './user';
+import restaurant from './restaurant';
 
 export default combineReducers({
   counter,
-  user
+  user,
+  restaurant
 });
