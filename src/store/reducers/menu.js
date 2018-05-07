c19f029ab921aaf4442475c154ce1753debd3878
MegrezZhu <mystery490815101@gmail.com>
2018-05-07 22:49:34 +0800

    total price

--- src/store/reducers/menu.js
+++ src/store/reducers/menu.js
@@ -1,15 +1,16 @@
 import { handleActions } from 'redux-actions';
 import { MENU_UPDATE, MENU_MODIFY_ITEM_COUNT } from '../types/menu';
+import { List } from 'immutable';
 
 export default handleActions({
   [MENU_UPDATE] (state, action) {
     const data = action.payload;
-    const idMap = {};
+    const idMap = new Map();
     let count = 0;
     for (const arr of Object.values(data)) {
       for (const item of arr) {
         item.context.id = ++count;
-        idMap[count] = item;
+        idMap.set(count, item);
       }
     }
 
@@ -17,25 +18,25 @@ export default handleActions({
       ...state,
       data: action.payload,
       categories: [...Object.keys(action.payload)],
-      idMap
+      idMap: idMap
     };
   },
+
   [MENU_MODIFY_ITEM_COUNT] (state, action) {
     const { id, modifier } = action.payload;
-    const newItem = { ...state.idMap[id] };
+    const newItem = { ...state.idMap.get(id) };
     newItem.context.count += modifier;
     newItem.context.count = Math.min(99, newItem.context.count);
     newItem.context.count = Math.max(0, newItem.context.count);
+    const newMap = new Map(state.idMap);
+    newMap.set(id, newItem);
     return {
       ...state,
-      idMap: {
-        ...state.idMap,
-        [id]: newItem
-      }
+      idMap: newMap
     };
   }
 }, {
   data: {},
   categories: [],
-  idMap: {}
+  idMap: new Map()
 });
