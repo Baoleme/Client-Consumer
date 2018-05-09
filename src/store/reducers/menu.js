c9a4c83fbab76cd2f8024d03bcf4d30399fa6822
MegrezZhu <mystery490815101@gmail.com>
2018-05-09 16:50:02 +0800

    refactor

--- src/store/reducers/menu.js
+++ src/store/reducers/menu.js
@@ -1,6 +1,84 @@
 import { handleActions } from 'redux-actions';
-import { MENU_UPDATE, MENU_MODIFY_ITEM_COUNT, MENU_EMPTY_BASKET, MENU_OPEN_DETAIL, MENU_CLOSE_DETAIL } from '../types/menu';
-import { List } from 'immutable';
+import {
+  MENU_UPDATE,
+  MENU_MODIFY_ITEM_COUNT,
+  MENU_EMPTY_BASKET,
+  MENU_OPEN_DETAIL,
+  MENU_CLOSE_DETAIL,
+  MENU_MODIFY_ITEM_COUNT_WITH_SPEC
+} from '../types/menu';
+import { Map as IMap } from 'immutable';
+
+/**
+ * a ['{{id}}#{{spec1}}/{{spec2}}.../{{specn}}' -> {count, unitPrice}] map
+ */
+class ImmutableBasket {
+  constructor (data = null) {
+    this.map = new IMap(data);
+  }
+
+  /**
+   *
+   * @param {number} id item id
+   * @param {object} spec key: option name, value: selected option
+   */
+  plus (id, spec) {
+    const uniqKey = this._genKey(id, spec);
+    const oldV = this.map.get(uniqKey) || { count: 0, unitPrice: this._calculateUnitPrice(id, spec), spec };
+    const newV = { ...oldV, count: this._clamp(oldV.count + 1) };
+    return new ImmutableBasket(
+      this.map.set(uniqKey, newV)
+    );
+  }
+
+  minus (id, spec) {
+    const uniqKey = this._genKey(id, spec);
+    const oldV = this.map.get(uniqKey) || { count: 0, unitPrice: this._calculateUnitPrice(id, spec), spec };
+    const newV = { ...oldV, count: this._clamp(oldV.count - 1) };
+    if (newV.count <= 0) return new ImmutableBasket(this.map.delete(uniqKey));
+    return new ImmutableBasket(
+      this.map.set(uniqKey, newV)
+    );
+  }
+
+  clear () {
+    return new ImmutableBasket();
+  }
+
+  toArray () {
+    return [...Object.entries(this.map.toObject())].map(([uniqKey, { unitPrice, count, spec }]) => {
+      const [, id, specStr] = uniqKey.match(/(\d+)#(.*)/);
+      return {
+        id: Number(id),
+        specStr,
+        spec,
+        count,
+        unitPrice
+      };
+    });
+  }
+
+  size () {
+    return this.map.size;
+  }
+
+  _genKey (id, spec) {
+    const specStr = spec ? [...Object.values(spec)].join('/') : '';
+    return `${id}#${specStr}`;
+  }
+
+  _clone () {
+    return new ImmutableBasket(this.map);
+  }
+
+  _clamp (v) {
+    return Math.min(99, Math.max(0, v));
+  }
+
+  _calculateUnitPrice (id, spec) {
+    return 12; // TODO:
+  }
+}
 
 export default handleActions({
   [MENU_UPDATE] (state, action) {
@@ -26,24 +104,17 @@ export default handleActions({
     const newState = { ...state };
 
     const { id, modifier } = action.payload;
-    const oldItem = state.idMap.get(id);
-    const newItem = { ...oldItem };
+    const item = state.idMap.get(id);
 
-    const oldCount = oldItem.context.count;
-    let newCount = oldCount + modifier;
-    newCount = Math.min(99, newCount);
-    newCount = Math.max(0, newCount);
-    newItem.context.count = newCount;
-    const newMap = new Map(state.idMap);
-    newMap.set(id, newItem);
+    const oldCount = item.context.count;
+    item.context.count = state.basket._clamp(oldCount + modifier);
 
-    if (oldCount !== 0 && newCount === 0) {
-      // deleted from basket
-      newState.basket = newState.basket.delete(newState.basket.indexOf(id));
-    } else if (oldCount === 0 && newCount !== 0) {
-      // added to basket
-      newState.basket = newState.basket.push(id);
+    if (modifier === 1) { // add
+      newState.basket = newState.basket.plus(id, null);
+    } else {
+      newState.basket = newState.basket.minus(id, null);
     }
+
     return newState;
   },
 
@@ -54,7 +125,7 @@ export default handleActions({
 
     return {
       ...state,
-      basket: new List()
+      basket: state.basket.clear()
     };
   },
   [MENU_OPEN_DETAIL] (state, action) {
@@ -73,6 +144,6 @@ export default handleActions({
   data: {},
   categories: [],
   idMap: new Map(),
-  basket: new List(),
+  basket: new ImmutableBasket(),
   detailingItem: null // item id
 });
