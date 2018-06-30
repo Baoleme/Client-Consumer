97e5628d37940dfffaca4afab0ce9488d9a50b4a
MegrezZhu <mystery490815101@gmail.com>
2018-06-30 22:02:31 +0800

    bug fixes

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -4,6 +4,11 @@ import { ORDER_CREATE, ORDER_SELECT_DETAILING } from '../types';
 
 export default handleActionsWithoutError({
   [ORDER_CREATE] (state, action) {
+    for (const order of action.payload) {
+      if (order['state_record']) {
+        order.state = order['state_record'][order['state_record'].length - 1].state;
+      }
+    }
     return {
       ...state,
       data: state.data.unshift(...action.payload)
