3782747f45f007f1962ae6fbb5c7787ad5bd264c
MegrezZhu <mystery490815101@gmail.com>
2018-07-07 21:57:12 +0800

    added order polling

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,6 +1,6 @@
-import { ORDER_CREATE, ORDER_SELECT_DETAILING } from '../types';
+import { ORDER_CREATE, ORDER_SELECT_DETAILING, ORDER_REFRESH } from '../types';
 import { createAction } from 'redux-actions';
 
 export const createOrder = createAction(ORDER_CREATE, (...orders) => orders);
-
+export const refreshOrder = createAction(ORDER_REFRESH, (...orders) => orders);
 export const selectOrderDetail = createAction(ORDER_SELECT_DETAILING);
